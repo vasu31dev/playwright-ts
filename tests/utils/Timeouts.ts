2a9c691238fd74e85349c23e16b1cb5b38a1fb2c
vasu31dev <vasu31dev@gmail.com>
2023-07-20 09:29:46 -0400

    Added Timeouts.ts file

---
+++ tests/utils/Timeouts.ts
@@ -0,0 +1,7 @@
+// Timeouts
+export const INSTANT_TIMEOUT = 1000;
+export const SMALL_TIMEOUT = 5 * 1000;
+export const STANDARD_TIMEOUT = 15 * 1000;
+export const BIG_TIMEOUT = 30 * 1000;
+export const MAX_TIMEOUT = 60 * 1000;
+export const TEST_TIMEOUT = 2 * 60 * 1000;
