d2ef9faa9ea57db98f82aa4e76c762665b713f17
vasu31dev <vasu31dev@gmail.com>
2023-08-02 10:15:49 -0400

    Added isElementAttached, isElementHidden, isElementChecked, getLocatorCount functions and updated isElementVisible function with proper formatting

--- tests/utils/ElementUtils.ts
+++ tests/utils/ElementUtils.ts
@@ -1,22 +1,20 @@
-import { Locator } from "@playwright/test";
-import { getPage } from "@PageFactory";
-import {
-  TimeoutOption,
-} from "@Types";
-import { getAllLocators, getLocator } from "@LocatorUtils";
-import { SMALL_TIMEOUT } from "@Timeouts";
+import { Locator } from '@playwright/test';
+import { getPage } from '@PageFactory';
+import { TimeoutOption } from '@Types';
+import { getAllLocators, getLocator } from '@LocatorUtils';
+import { INSTANT_TIMEOUT, SMALL_TIMEOUT } from '@Timeouts';
 
 // Text
 export async function getText(
   input: string | Locator,
-  options?: TimeoutOption
+  options?: TimeoutOption,
 ): Promise<string> {
   const locator = getLocator(input);
   return await locator.innerText(options);
 }
 
 export async function getAllTexts(
-  input: string | Locator
+  input: string | Locator,
 ): Promise<Array<string>> {
   const locator = getLocator(input);
   return await locator.allInnerTexts();
@@ -24,7 +22,7 @@ export async function getAllTexts(
 
 export async function getInputValue(
   input: string | Locator,
-  options?: TimeoutOption
+  options?: TimeoutOption,
 ): Promise<string> {
   const locator = getLocator(input);
   return await locator.inputValue(options);
@@ -32,18 +30,18 @@ export async function getInputValue(
 
 export async function getAllInputValues(
   input: string | Locator,
-  options?: TimeoutOption
+  options?: TimeoutOption,
 ): Promise<Array<string>> {
   const locators = await getAllLocators(input);
   return Promise.all(
-    locators.map((locator) => getInputValue(locator, options))
+    locators.map((locator) => getInputValue(locator, options)),
   );
 }
 
 export async function getAttribute(
   input: string | Locator,
   attributeName: string,
-  options?: TimeoutOption
+  options?: TimeoutOption,
 ): Promise<null | string> {
   const locator = getLocator(input);
   return await locator.getAttribute(attributeName, options);
@@ -53,37 +51,108 @@ export async function saveStorageState(path?: string): Promise<void> {
   await getPage().context().storageState({ path: path });
 }
 
+export async function getLocatorCount(
+  input: string | Locator,
+  options?: TimeoutOption,
+): Promise<number> {
+  const timeoutInMs = options?.timeout || INSTANT_TIMEOUT;
+  try {
+    if (await isElementAttached(input, { timeout: timeoutInMs })) {
+      return (await getAllLocators(input)).length;
+    }
+  } catch (error) {
+    console.log(
+      `getLocatorCount- ${
+        error instanceof Error ? error.message : String(error)
+      }`,
+    );
+  }
+  return 0;
+}
+
 //Conditions
+export async function isElementAttached(
+  input: string | Locator,
+  options?: TimeoutOption,
+): Promise<boolean> {
+  const locator = getLocator(input); // Assuming getLocator returns a Playwright Locator
+  const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
+
+  try {
+    await locator.waitFor({ state: 'attached', timeout: timeoutInMs });
+    return true;
+  } catch (error) {
+    console.log(
+      `isElementAttached- ${
+        error instanceof Error ? error.message : String(error)
+      }`,
+    );
+    return false;
+  }
+}
+
 export async function isElementVisible(
   input: string | Locator,
-  options?: TimeoutOption
+  options?: TimeoutOption,
 ): Promise<boolean> {
   const locator = getLocator(input);
-  const timeInMs = options?.timeout || SMALL_TIMEOUT;
+  const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
   const startTime = Date.now();
   try {
-    while (
-      !(await locator.isVisible(options)) &&
-      Date.now() - startTime < timeInMs
-    ) {
+    while (Date.now() - startTime < timeoutInMs) {
+      if (await locator.isVisible(options)) {
+        return true;
+      }
       await new Promise((resolve) => setTimeout(resolve, 100));
     }
   } catch (error) {
     console.log(
-      `isElementVisible1- ${
+      `isElementVisible- ${
         error instanceof Error ? error.message : String(error)
-      }`
+      }`,
     );
-    return false;
   }
+  return false;
+}
+
+export async function isElementHidden(
+  input: string | Locator,
+  options?: TimeoutOption,
+): Promise<boolean> {
+  const locator = getLocator(input);
+  const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
+  const startTime = Date.now();
   try {
-    return await locator.isVisible();
+    while (Date.now() - startTime < timeoutInMs) {
+      if (await locator.isHidden(options)) {
+        return true;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
   } catch (error) {
     console.log(
-      `isElementVisible2- ${
+      `isElementHidden- ${
         error instanceof Error ? error.message : String(error)
-      }`
+      }`,
     );
-    return false;
   }
+  return false;
+}
+
+export async function isElementChecked(
+  input: string | Locator,
+  options?: TimeoutOption,
+): Promise<boolean> {
+  try {
+    if (await isElementVisible(input, options)) {
+      return await getLocator(input).isChecked(options);
+    }
+  } catch (error) {
+    console.log(
+      `isElementChecked- ${
+        error instanceof Error ? error.message : String(error)
+      }`,
+    );
+  }
+  return false;
 }
