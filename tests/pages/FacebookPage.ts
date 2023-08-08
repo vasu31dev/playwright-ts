2065f9527b87e022e011a88a5e433728ec398ca6
vidyavemulapalli <vidyavemulapalli11@gmail.com>
2023-08-08 14:56:51 +0100

    added spec file for switch pages demo

---
+++ tests/pages/FacebookPage.ts
@@ -0,0 +1,39 @@
+import { click, clickAndNavigate, gotoURL } from '@ActionUtils';
+import { expectPageToHaveURL } from '@AssertUtils';
+import { getLocatorByText } from '@LocatorUtils';
+
+const instagramLink = () => getLocatorByText('Instagram', { exact: true });
+const privacyPolicyLink = () => getLocatorByText('Privacy Policy');
+const metaQuestLink = () => getLocatorByText('Meta Quest');
+
+export async function navigatetoFacebookHomePage() {
+  await gotoURL('https://www.facebook.com/');
+}
+
+export async function verifyFacebookPageURL() {
+  await expectPageToHaveURL(new RegExp('https://www.facebook.com/'));
+}
+
+export async function clickFacebookInstagramFooterLink() {
+  await click(instagramLink());
+}
+
+export async function verifyInstagaramPageURL() {
+  await expectPageToHaveURL(new RegExp('https://www.instagram.com/'));
+}
+
+export async function clickFacebookPrivacyPolicyFooterLink() {
+  await clickAndNavigate(privacyPolicyLink());
+}
+
+export async function verifyPrivacyPolicyPageURL() {
+  await expectPageToHaveURL(new RegExp('www.facebook.com/privacy/policy/'));
+}
+
+export async function clickFacebookMetaQuestFooterLink() {
+  await click(metaQuestLink());
+}
+
+export async function verifyMetaQuestpageURL() {
+  await expectPageToHaveURL(new RegExp('meta.com/quest/'));
+}
