ed90c0bc1aa1cf1af5107289296498bcf50f7749
vasu31dev <vasu31dev@gmail.com>
2023-08-08 11:59:48 -0400

    Added proper JsDoc Descriptions for all the framework setup files

--- tests/setup/GlobalSetup.ts
+++ tests/setup/GlobalSetup.ts
@@ -1,7 +1,10 @@
 /**
- * Global setup function to be executed before all tests start.
- * You can add any initialization setup code here.
+ * GlobalSetup.ts: This module is responsible for setting up the global state before all tests start.
+ * It includes a default export function that runs before all tests, setting up any necessary global context.
+ * By centralizing these setup operations, it ensures a consistent starting point for all tests, improving test reliability.
+ * You can add any initialization setup code within this function.
  */
+
 export default () => {
   // console.log("Add any initialization setup here");
 };
--- tests/setup/PageSetup.ts
+++ tests/setup/PageSetup.ts
@@ -1,3 +1,10 @@
+/**
+ * PageSetup.ts: This module is responsible for setting up the initial state of a page before each test.
+ * It includes a hook that runs before each test, setting the page context. By centralizing these setup operations,
+ * it ensures a consistent starting point for each test, improving test reliability. It also exports a base test object
+ * with a beforeEach hook already set up. This can be used to define tests with the page context set up.
+ */
+
 import { Page, test as baseTest } from '@playwright/test';
 import { setPage } from './PageFactory';
 
@@ -12,6 +19,5 @@ baseTest.beforeEach(({ page }: { page: Page }) => {
 /**
  * The base test object with a beforeEach hook already set up.
  * This can be used to define tests with the page context set up.
- * @type {import('@playwright/test').TestType}
  */
 export const test = baseTest;
