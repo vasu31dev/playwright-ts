f856be1dda8543a1646158b875af166f495e6b37
vasu31dev <vasu31dev@gmail.com>
2023-08-08 13:00:22 -0400

    Updated the description of TimeoutConstants.ts JsDoc

--- tests/utils/TimeoutConstants.ts
+++ tests/utils/TimeoutConstants.ts
@@ -1,5 +1,5 @@
 /**
- * TimeoutConstants.ts: This module provides Timeout constants that can be used to over ride various actions, conditional statements and assertions.
+ * TimeoutConstants.ts: This module provides Timeout constants that can be used to override various actions, conditional statements and assertions.
  * Instead of hard coding the timeout when overriding any utility functions, use these Timeout constants.
  */
 
