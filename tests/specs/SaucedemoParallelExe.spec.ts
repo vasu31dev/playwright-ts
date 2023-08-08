8f936e902eace41f51534ad4774fd8238aacb935
vidyavemulapalli <vidyavemulapalli11@gmail.com>
2023-08-08 13:48:16 +0100

    added parallel exe spec file

---
+++ tests/specs/SaucedemoParallelExe.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@PageSetup';
+import * as LoginPage from 'tests/pages/SauceDemoLoginPage';
+import * as ProductsPage from 'tests/pages/SauceDemoProductsPage';
+
+//If you want to run the tests in parallel, you can use the test.describe.configure() method to set the mode to parallel. By default, tests are run sequentially.
+test.describe.configure({ mode: 'parallel' });
+
+test.describe('Saucedemo tests success and failures cases', () => {
+  test('Saucedemo tests - Successful test', async () => {
+    await LoginPage.navigateToSauceDemoLoginPage();
+    await LoginPage.logInSuccessfully();
+    //verifying products page is displayed only on successful login
+    await ProductsPage.verifyProductsPageDisplayed();
+  });
+
+  test('Saucedemo tests - Failure test', async () => {
+    await LoginPage.navigateToSauceDemoLoginPage();
+    await LoginPage.failureLogin();
+    //verifying products page is displayed only on successful login
+    await ProductsPage.verifyProductsPageDisplayed();
+  });
+});
