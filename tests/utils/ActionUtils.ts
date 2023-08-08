00ab2231bb67ec253973c7dd6e7913834fcc527b
vasu31dev <vasu31dev@gmail.com>
2023-08-08 11:04:24 -0400

    Added Jsdoc comments to ActionUtils.ts file

--- tests/utils/ActionUtils.ts
+++ tests/utils/ActionUtils.ts
@@ -1,3 +1,7 @@
+/**
+ * This module provides a set of utility functions for performing various actions in Playwright tests.
+ * These actions include navigation, interaction with page elements, handling of dialogs, and more.
+ */
 import { Dialog, Locator, Response } from '@playwright/test';
 import { getPage } from '@PageFactory';
 import {
@@ -21,12 +25,26 @@ import { STANDARD_TIMEOUT } from '@TimeoutConstants';
 import { LOADSTATE } from 'playwright.config';
 import { getLocator } from '@LocatorUtils';
 
-// Navigations
+/**
+ * This Navigating section contains functions for navigating within a web page or between web pages.
+ * These functions include going to a URL, waiting for a page to load, reloading a page, and going back to a previous page.
+ */
+
+/**
+ * Navigates to the specified URL.
+ * @param {string} path - The URL to navigate to.
+ * @param {GotoOptions} options - The navigation options.
+ * @returns {Promise<null | Response>} - The navigation response.
+ */
 export async function gotoURL(path: string, options: GotoOptions = { waitUntil: LOADSTATE }): Promise<null | Response> {
   return await getPage().goto(path, options);
 }
 
-// Helper function to wait for a specific page load state
+/**
+ * Waits for a specific page load state.
+ * @param {NavigationOptions} options - The navigation options.
+ * @returns {Promise<void>}
+ */
 export async function waitForPageLoadState(options?: NavigationOptions): Promise<void> {
   let waitUntil: WaitForLoadStateOptions = LOADSTATE;
 
@@ -36,27 +54,59 @@ export async function waitForPageLoadState(options?: NavigationOptions): Promise
 
   await getPage().waitForLoadState(waitUntil);
 }
+
+/**
+ * Reloads the current page.
+ * @param {NavigationOptions} options - The navigation options.
+ * @returns {Promise<void>}
+ */
 export async function reloadPage(options?: NavigationOptions): Promise<void> {
   await Promise.all([getPage().reload(options), getPage().waitForEvent('framenavigated')]);
   await waitForPageLoadState(options);
 }
 
+/**
+ * Navigates back to the previous page.
+ * @param {NavigationOptions} options - The navigation options.
+ * @returns {Promise<void>}
+ */
 export async function goBack(options?: NavigationOptions): Promise<void> {
   await Promise.all([getPage().goBack(options), getPage().waitForEvent('framenavigated')]);
   await waitForPageLoadState(options);
 }
 
+/**
+ * Waits for a specified amount of time.
+ * @param {number} ms - The amount of time to wait in milliseconds.
+ * @returns {Promise<void>}
+ */
 export async function wait(ms: number): Promise<void> {
   // eslint-disable-next-line playwright/no-wait-for-timeout
   await getPage().waitForTimeout(ms);
 }
 
-// Actions
+/**
+ * This Actions contains functions for interacting with elements on a web page.
+ * These functions include clicking, filling input fields, typing, clearing input fields, checking and unchecking checkboxes, selecting options in dropdowns, and more.
+ */
+
+/**
+ * Clicks on a specified element.
+ * @param {string | Locator} input - The element to click on.
+ * @param {ClickOptions} options - The click options.
+ * @returns {Promise<void>}
+ */
 export async function click(input: string | Locator, options?: ClickOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.click(options);
 }
 
+/**
+ * Clicks on a specified element and waits for navigation.
+ * @param {string | Locator} input - The element to click on.
+ * @param {ClickOptions} options - The click options.
+ * @returns {Promise<void>}
+ */
 export async function clickAndNavigate(input: string | Locator, options?: ClickOptions): Promise<void> {
   const timeout = options?.timeout || STANDARD_TIMEOUT;
   await Promise.all([click(input, options), getPage().waitForEvent('framenavigated', { timeout: timeout })]);
@@ -65,42 +115,95 @@ export async function clickAndNavigate(input: string | Locator, options?: ClickO
   });
 }
 
+/**
+ * Fills a specified element with a value.
+ * @param {string | Locator} input - The element to fill.
+ * @param {string} value - The value to fill the element with.
+ * @param {FillOptions} options - The fill options.
+ * @returns {Promise<void>}
+ */
 export async function fill(input: string | Locator, value: string, options?: FillOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.fill(value, options);
 }
 
+/**
+ * Fills a specified element with a value and presses Enter.
+ * @param {string | Locator} input - The element to fill.
+ * @param {string} value - The value to fill the element with.
+ * @param {FillOptions} options - The fill options.
+ * @returns {Promise<void>}
+ */
 export async function fillAndEnter(input: string | Locator, value: string, options?: FillOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.fill(value, options);
   await locator.press('Enter');
 }
 
+/**
+ * Types a value into a specified element.
+ * @param {string | Locator} input - The element to type into.
+ * @param {string} value - The value to type.
+ * @param {TypeOptions} options - The type options.
+ * @returns {Promise<void>}
+ */
 export async function type(input: string | Locator, value: string, options?: TypeOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.type(value, options);
 }
 
+/**
+ * Clears the value of a specified element.
+ * @param {string | Locator} input - The element to clear.
+ * @param {ClearOptions} options - The clear options.
+ * @returns {Promise<void>}
+ */
 export async function clear(input: string | Locator, options?: ClearOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.clear(options);
 }
 
+/**
+ * Checks a specified checkbox or radio button.
+ * @param {string | Locator} input - The checkbox or radio button to check.
+ * @param {CheckOptions} options - The check options.
+ * @returns {Promise<void>}
+ */
 export async function check(input: string | Locator, options?: CheckOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.check(options);
 }
 
+/**
+ * Unchecks a specified checkbox or radio button.
+ * @param {string | Locator} input - The checkbox or radio button to uncheck.
+ * @param {CheckOptions} options - The uncheck options.
+ * @returns {Promise<void>}
+ */
 export async function uncheck(input: string | Locator, options?: CheckOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.uncheck(options);
 }
 
+/**
+ * Selects an option in a dropdown by its value.
+ * @param {string | Locator} input - The dropdown to select an option in.
+ * @param {string} value - The value of the option to select.
+ * @param {SelectOptions} options - The select options.
+ * @returns {Promise<void>}
+ */
 export async function selectByValue(input: string | Locator, value: string, options?: SelectOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.selectOption({ value: value }, options);
 }
 
+/**
+ * Selects options in a dropdown by their values.
+ * @param {string | Locator} input - The dropdown to select options in.
+ * @param {Array<string>} value - The values of the options to select.
+ * @param {SelectOptions} options - The select options.
+ * @returns {Promise<void>}
+ */
 export async function selectByValues(
   input: string | Locator,
   value: Array<string>,
@@ -110,17 +213,42 @@ export async function selectByValues(
   await locator.selectOption(value, options);
 }
 
+/**
+ * Selects an option in a dropdown by its text.
+ * @param {string | Locator} input - The dropdown to select an option in.
+ * @param {string} text - The text of the option to select.
+ * @param {SelectOptions} options - The select options.
+ * @returns {Promise<void>}
+ */
 export async function selectByText(input: string | Locator, text: string, options?: SelectOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.selectOption({ label: text }, options);
 }
 
+/**
+ * Selects an option in a dropdown by its index.
+ * @param {string | Locator} input - The dropdown to select an option in.
+ * @param {number} index - The index of the option to select.
+ * @param {SelectOptions} options - The select options.
+ * @returns {Promise<void>}
+ */
 export async function selectByIndex(input: string | Locator, index: number, options?: SelectOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.selectOption({ index: index }, options);
 }
 
-//todo: Optmize Alert functions with DRY principle
+/**
+ * This Alerts section contains functions for handling alert dialogs.
+ * These functions include accepting and dismissing alerts, and getting the text of an alert.
+ * Note: These functions currently have some repetition and could be optimized by applying the DRY (Don't Repeat Yourself) principle.
+ */
+
+/**
+ * Accepts an alert dialog.
+ * @param {string | Locator} input - The element to click to trigger the alert.
+ * @param {string} promptText - The text to enter into a prompt dialog.
+ * @returns {Promise<string>} - The message of the dialog.
+ */
 export async function acceptAlert(input: string | Locator, promptText?: string): Promise<string> {
   const locator = getLocator(input);
   let dialogMessage = '';
@@ -134,6 +262,11 @@ export async function acceptAlert(input: string | Locator, promptText?: string):
   return dialogMessage;
 }
 
+/**
+ * Dismisses an alert dialog.
+ * @param {string | Locator} input - The element to click to trigger the alert.
+ * @returns {Promise<string>} - The message of the dialog.
+ */
 export async function dismissAlert(input: string | Locator): Promise<string> {
   const locator = getLocator(input);
   let dialogMessage = '';
@@ -147,6 +280,11 @@ export async function dismissAlert(input: string | Locator): Promise<string> {
   return dialogMessage;
 }
 
+/**
+ * Gets the text of an alert dialog.
+ * @param {string | Locator} input - The element to click to trigger the alert.
+ * @returns {Promise<string>} - The message of the dialog.
+ */
 export async function getAlertText(input: string | Locator): Promise<string> {
   const locator = getLocator(input);
   let dialogMessage = '';
@@ -160,16 +298,35 @@ export async function getAlertText(input: string | Locator): Promise<string> {
   return dialogMessage;
 }
 
+/**
+ * Hovers over a specified element.
+ * @param {string | Locator} input - The element to hover over.
+ * @param {HoverOptions} options - The hover options.
+ * @returns {Promise<void>}
+ */
 export async function hover(input: string | Locator, options?: HoverOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.hover(options);
 }
 
+/**
+ * Focuses on a specified element.
+ * @param {string | Locator} input - The element to focus on.
+ * @param {TimeoutOption} options - The timeout options.
+ * @returns {Promise<void>}
+ */
 export async function focus(input: string | Locator, options?: TimeoutOption): Promise<void> {
   const locator = getLocator(input);
   await locator.focus(options);
 }
 
+/**
+ * Drags and drops a specified element to a destination.
+ * @param {string | Locator} input - The element to drag.
+ * @param {string | Locator} dest - The destination to drop the element at.
+ * @param {DragOptions} options - The drag options.
+ * @returns {Promise<void>}
+ */
 export async function dragAndDrop(
   input: string | Locator,
   dest: string | Locator,
@@ -180,11 +337,23 @@ export async function dragAndDrop(
   await drag.dragTo(drop, options);
 }
 
+/**
+ * Double clicks on a specified element.
+ * @param {string | Locator} input - The element to double click on.
+ * @param {DoubleClickOptions} options - The double click options.
+ * @returns {Promise<void>}
+ */
 export async function doubleClick(input: string | Locator, options?: DoubleClickOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.dblclick(options);
 }
 
+/**
+ * Downloads a file from a specified element.
+ * @param {string | Locator} input - The element to download the file from.
+ * @param {string} path - The path to save the downloaded file to.
+ * @returns {Promise<void>}
+ */
 export async function downloadFile(input: string | Locator, path: string): Promise<void> {
   const locator = getLocator(input);
   const downloadPromise = getPage().waitForEvent('download');
@@ -196,17 +365,40 @@ export async function downloadFile(input: string | Locator, path: string): Promi
   await download.saveAs(path);
 }
 
+/**
+ * Uploads files to a specified element.
+ * @param {string | Locator} input - The element to upload files to.
+ * @param {UploadValues} path - The files to upload.
+ * @param {UploadOptions} options - The upload options.
+ * @returns {Promise<void>}
+ */
 export async function uploadFiles(input: string | Locator, path: UploadValues, options?: UploadOptions): Promise<void> {
   const locator = getLocator(input);
   await locator.setInputFiles(path, options);
 }
 
+/**
+ * Scrolls a specified element into view.
+ * @param {string | Locator} input - The element to scroll into view.
+ * @param {TimeoutOption} options - The timeout options.
+ * @returns {Promise<void>}
+ */
 export async function scrollLocatorIntoView(input: string | Locator, options?: TimeoutOption): Promise<void> {
   const locator = getLocator(input);
   await locator.scrollIntoViewIfNeeded(options);
 }
 
-//JS
+/**
+ * This JS section contains functions that use JavaScript to interact with elements on a web page.
+ * These functions include clicking on an element using JavaScript.
+ */
+
+/**
+ * Clicks on a specified element using JavaScript.
+ * @param {string | Locator} input - The element to click on.
+ * @param {TimeoutOption} options - The timeout options.
+ * @returns {Promise<void>}
+ */
 export async function clickByJS(input: string | Locator, options?: TimeoutOption): Promise<void> {
   const locator = getLocator(input);
   await locator.evaluate('el => el.click()', options);
