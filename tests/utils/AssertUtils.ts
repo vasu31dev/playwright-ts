cfcf59e82bdd8cd40e9f4458801129ad909a7a5a
vasu31dev <vasu31dev@gmail.com>
2023-08-08 10:41:54 -0400

    Added Jsdoc comments to AssertUtils.ts file

--- tests/utils/AssertUtils.ts
+++ tests/utils/AssertUtils.ts
@@ -1,74 +1,131 @@
+/**
+ * This module contains utility functions for assertions in tests.
+ * All expect assertions will dynamically wait until either the expect timeout specified in the
+ * playwright.config is reached or the condition becomes true.
+ * @module AssertUtils
+ */
+
 import { Expect, Locator, TestInfo, expect } from '@playwright/test';
-import { getPage } from '@PageFactory';
 import { ExpectOptions, ExpectTextOptions, SoftOption } from '@Types';
 import { getLocator } from '@LocatorUtils';
+import { getPage } from '@PageFactory';
 
+/**
+ * Returns an Expect object configured with the given soft option.
+ * @param {SoftOption} options - The soft option to configure the Expect object with.
+ * @returns {Expect} - The configured Expect object.
+ */
 function getExpectWithSoftOption(options?: SoftOption): Expect {
   return expect.configure({ soft: options?.soft });
 }
 
+/**
+ * Returns a Locator object and an Expect object configured with the given soft option.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {SoftOption} options - The soft option to configure the Expect object with.
+ * @returns {Object} - An object containing the Locator and Expect objects.
+ */
 function getLocatorAndAssert(input: string | Locator, options?: SoftOption): { locator: Locator; assert: Expect } {
   const locator = getLocator(input);
   const assert = getExpectWithSoftOption(options);
   return { locator, assert };
 }
 
-/** Use this to assert all the soft assertions  */
+/**
+ * Use this function to assert all the soft assertions.
+ * @param {TestInfo} testInfo - The TestInfo object containing the test's information.
+ */
 export function assertAllSoftAssertions(testInfo: TestInfo) {
   expect(testInfo.errors).toHaveLength(0);
 }
 
-/**
- * All expect assertions will dynamically wait until either the expect timeout specified in the
- * playwright.config is reached or the condition becomes true.
- * @param {string | locator} input - Either a string (selector) or a Locator object
- */
+/* Locator Assertions */
 
-//Locator Assertions
-/* Check if the given element is not present in DOM, or not visible */
+/**
+ * Asserts that the given element is not present in the DOM or is Hidden.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeHidden(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeHidden(options);
 }
 
-/* Check if the given element is present in DOM, and visible */
+/**
+ * Asserts that the given element is present in the DOM and visible.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeVisible(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeVisible(options);
 }
 
-/* Check if the given element is present in DOM */
+/**
+ * Asserts that the given element is present in the DOM.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeAttached(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeAttached(options);
 }
 
-/* Check if the given element is present in DOM and Visible in the view port of the page */
+/**
+ * Asserts that the given element is present in the DOM and visible in the viewport of the page.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeInViewport(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeInViewport(options);
 }
 
+/**
+ * Asserts that the given element is checked.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeChecked(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeChecked(options);
 }
 
+/**
+ * Asserts that the given element is not checked.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementNotToBeChecked(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).not.toBeChecked(options);
 }
 
+/**
+ * Asserts that the given element is disabled.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeDisabled(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeDisabled(options);
 }
 
+/**
+ * Asserts that the given element is enabled.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeEnabled(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeEnabled(options);
 }
 
+/**
+ * Asserts that the given element is editable.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToBeEditable(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeEditable(options);
@@ -76,9 +133,9 @@ export async function expectElementToBeEditable(input: string | Locator, options
 
 /**
  * Asserts that the element equals the provided string or string array or regular expression.
- * @param {string | locator} input - Either a string (selector) or a Locator object
- * @param {string| string[] | RegExp} text - The string, string array or regular expression to match against the
- *   element's text.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string | string[] | RegExp} text - The string, string array or regular expression to match against the element's text.
+ * @param {ExpectOptions & ExpectTextOptions} options - The options to pass to the expect function.
  */
 export async function expectElementToHaveText(
   input: string | Locator,
@@ -89,6 +146,12 @@ export async function expectElementToHaveText(
   await assert(locator, options).toHaveText(text, options);
 }
 
+/**
+ * Asserts that the element does not equal the provided string or string array or regular expression.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string | string[] | RegExp} text - The string, string array or regular expression to match against the element's text.
+ * @param {ExpectOptions & ExpectTextOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementNotToHaveText(
   input: string | Locator,
   text: string | RegExp | Array<string | RegExp>,
@@ -98,6 +161,12 @@ export async function expectElementNotToHaveText(
   await assert(locator, options).not.toHaveText(text, options);
 }
 
+/**
+ * Asserts that the element contains the provided string or string array or regular expression.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string | string[] | RegExp} text - The string, string array or regular expression to match against the element's text.
+ * @param {ExpectOptions & ExpectTextOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToContainText(
   input: string | Locator,
   text: string | RegExp | Array<string | RegExp>,
@@ -107,6 +176,12 @@ export async function expectElementToContainText(
   await assert(locator, options).toContainText(text, options);
 }
 
+/**
+ * Asserts that the element does not contain the provided string or string array or regular expression.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string | string[] | RegExp} text - The string, string array or regular expression to match against the element's text.
+ * @param {ExpectOptions & ExpectTextOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementNotToContainText(
   input: string | Locator,
   text: string | RegExp | Array<string | RegExp>,
@@ -116,7 +191,12 @@ export async function expectElementNotToContainText(
   await assert(locator, options).not.toContainText(text, options);
 }
 
-/* Check if the given element points to input text box with given text or Regex */
+/**
+ * Asserts that the given element points to an input text box with the given text or Regex.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string | RegExp} text - The string or regular expression to match against the element's value.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToHaveValue(
   input: string | Locator,
   text: string | RegExp,
@@ -126,7 +206,12 @@ export async function expectElementToHaveValue(
   await assert(locator, options).toHaveValue(text, options);
 }
 
-/* Check if the given element points to multi-select/combobox (i.e. a select with the multiple attribute) and the specified values are selected. */
+/**
+ * Asserts that the given element points to a multi-select/combobox (i.e. a select with the multiple attribute) and the specified values are selected.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {Array<string | RegExp>} text - The array of strings or regular expressions to match against the element's values.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToHaveValues(
   input: string | Locator,
   text: Array<string | RegExp>,
@@ -136,13 +221,21 @@ export async function expectElementToHaveValues(
   await assert(locator, options).toHaveValues(text, options);
 }
 
-/* Check if the given element points to empty editable element or to a DOM node that has no text */
+/**
+ * Asserts that the given element points to an empty editable element or to a DOM node that has no text.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementValueToBeEmpty(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).toBeEmpty(options);
 }
 
-/* Check if the given element points to non-empty editable element or to a DOM node that has text */
+/**
+ * Asserts that the given element points to a non-empty editable element or to a DOM node that has text.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementValueNotToBeEmpty(input: string | Locator, options?: ExpectOptions): Promise<void> {
   const { locator, assert } = getLocatorAndAssert(input, options);
   await assert(locator, options).not.toBeEmpty(options);
@@ -150,9 +243,10 @@ export async function expectElementValueNotToBeEmpty(input: string | Locator, op
 
 /**
  * Asserts that an element has an attribute with the given value.
- * @param {string | locator} input - Either a string (selector) or a Locator object
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
  * @param {string} attribute - The attribute to check for.
- * @param {string} value - The value to match against the attribute.
+ * @param {string | RegExp} value - The value to match against the attribute.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
  */
 export async function expectElementToHaveAttribute(
   input: string | Locator,
@@ -164,6 +258,13 @@ export async function expectElementToHaveAttribute(
   await assert(locator, options).toHaveAttribute(attribute, value, options);
 }
 
+/**
+ * Asserts that an element has an attribute which contains the given value.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {string} attribute - The attribute to check for.
+ * @param {string | RegExp} value - The value to match against the attribute.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToContainAttribute(
   input: string | Locator,
   attribute: string,
@@ -174,6 +275,12 @@ export async function expectElementToContainAttribute(
   await assert(locator, options).toHaveAttribute(attribute, new RegExp(value), options);
 }
 
+/**
+ * Asserts that the given element has the specified count.
+ * @param {string | Locator} input - Either a string (selector) or a Locator object.
+ * @param {number} count - The count to match against the element's count.
+ * @param {ExpectOptions} options - The options to pass to the expect function.
+ */
 export async function expectElementToHaveCount(
   input: string | Locator,
   count: number,
@@ -183,7 +290,8 @@ export async function expectElementToHaveCount(
   await assert(locator, options).toHaveCount(count, options);
 }
 
-//Page Assertions
+/* Page Assertions */
+
 /**
  * Asserts that the current page URL matches exactly the provided URL or regular expression.
  * @param {string | RegExp} urlOrRegExp - The URL or regular expression to match against the current page URL.
