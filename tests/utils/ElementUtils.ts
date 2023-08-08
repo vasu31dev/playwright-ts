c7e49f220906c948ecbc54541f61cbaca95c1c14
vasu31dev <vasu31dev@gmail.com>
2023-08-08 09:45:03 -0400

    Added Jsdoc comments for ElementUtils.ts file

--- tests/utils/ElementUtils.ts
+++ tests/utils/ElementUtils.ts
@@ -5,27 +5,58 @@ import { getAllLocators, getLocator } from '@LocatorUtils';
 import { INSTANT_TIMEOUT, SMALL_TIMEOUT } from '@TimeoutConstants';
 import { waitForPageLoadState } from '@ActionUtils';
 
-// Text
+/* Use these to retrieve text/s in Conditional statements, not for Assertions unless the Web first Assertions don't meet your criteria */
+
+/**
+ * Returns the inner text of a Locator object.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<string>} - The inner text of the Locator.
+ */
 export async function getText(input: string | Locator, options?: TimeoutOption): Promise<string> {
   const locator = getLocator(input);
   return await locator.innerText(options);
 }
 
+/**
+ * Returns the inner text of all Locator objects.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @returns {Promise<Array<string>>} - The inner text of all Locator objects.
+ */
 export async function getAllTexts(input: string | Locator): Promise<Array<string>> {
   const locator = getLocator(input);
   return await locator.allInnerTexts();
 }
 
+/**
+ * Returns the input value of a Locator object.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<string>} - The input value of the Locator.
+ */
 export async function getInputValue(input: string | Locator, options?: TimeoutOption): Promise<string> {
   const locator = getLocator(input);
   return await locator.inputValue(options);
 }
 
+/**
+ * Returns the input values of all Locator objects.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<Array<string>>} - The input values of all Locator objects.
+ */
 export async function getAllInputValues(input: string | Locator, options?: TimeoutOption): Promise<Array<string>> {
   const locators = await getAllLocators(input);
   return Promise.all(locators.map(locator => getInputValue(locator, options)));
 }
 
+/**
+ * Returns the attribute of a Locator object.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {string} attributeName - The name of the attribute to get.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<null | string>} - The attribute of the Locator.
+ */
 export async function getAttribute(
   input: string | Locator,
   attributeName: string,
@@ -35,10 +66,20 @@ export async function getAttribute(
   return await locator.getAttribute(attributeName, options);
 }
 
+/**
+ * Saves the storage state of the page.
+ * @param {string} [path] - Optional path to save the storage state to.
+ * @returns {Promise<void>}
+ */
 export async function saveStorageState(path?: string): Promise<void> {
   await getPage().context().storageState({ path: path });
 }
 
+/**
+ * Returns the URL of the page.
+ * @param {NavigationOptions} [options] - Optional navigation options.
+ * @returns {Promise<string>} - The URL of the page.
+ */
 export async function getURL(options: NavigationOptions = { waitUntil: 'load' }): Promise<string> {
   try {
     await waitForPageLoadState(options);
@@ -49,6 +90,12 @@ export async function getURL(options: NavigationOptions = { waitUntil: 'load' })
   }
 }
 
+/**
+ * Returns the count of Locator objects.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<number>} - The count of Locator objects.
+ */
 export async function getLocatorCount(input: string | Locator, options?: TimeoutOption): Promise<number> {
   const timeoutInMs = options?.timeout || INSTANT_TIMEOUT;
   try {
@@ -61,7 +108,14 @@ export async function getLocatorCount(input: string | Locator, options?: Timeout
   return 0;
 }
 
-//Conditions
+/* Use these in Conditional statements, not for Assertions unless the web first Assertions don't meet your criteria */
+
+/**
+ * Checks if a Locator object is attached to DOM.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<boolean>} - True if the Locator is attached, false otherwise.
+ */
 export async function isElementAttached(input: string | Locator, options?: TimeoutOption): Promise<boolean> {
   const locator = getLocator(input); // Assuming getLocator returns a Playwright Locator
   const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
@@ -75,6 +129,12 @@ export async function isElementAttached(input: string | Locator, options?: Timeo
   }
 }
 
+/**
+ * Checks if a Locator object is attached to DOM and is visible.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<boolean>} - True if the Locator is visible, false otherwise.
+ */
 export async function isElementVisible(input: string | Locator, options?: TimeoutOption): Promise<boolean> {
   const locator = getLocator(input);
   const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
@@ -92,6 +152,12 @@ export async function isElementVisible(input: string | Locator, options?: Timeou
   return false;
 }
 
+/**
+ * Checks if a Locator object is hidden or not present in DOM.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<boolean>} - True if the Locator is hidden, false otherwise.
+ */
 export async function isElementHidden(input: string | Locator, options?: TimeoutOption): Promise<boolean> {
   const locator = getLocator(input);
   const timeoutInMs = options?.timeout || SMALL_TIMEOUT;
@@ -109,6 +175,12 @@ export async function isElementHidden(input: string | Locator, options?: Timeout
   return false;
 }
 
+/**
+ * Checks if a Locator object is checked.
+ * @param {string | Locator} input - The input to create the Locator from.
+ * @param {TimeoutOption} [options] - Optional timeout options.
+ * @returns {Promise<boolean>} - True if the Locator is checked, false otherwise.
+ */
 export async function isElementChecked(input: string | Locator, options?: TimeoutOption): Promise<boolean> {
   try {
     if (await isElementVisible(input, options)) {
