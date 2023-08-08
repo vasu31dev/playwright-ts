3760f574d7644cbb5d090760567f2bf4c7bddd10
vasu31dev <vasu31dev@gmail.com>
2023-08-08 09:51:15 -0400

    Added Jsdoc Comments for PageSetup.ts file

--- tests/setup/PageSetup.ts
+++ tests/setup/PageSetup.ts
@@ -1,8 +1,17 @@
-import { test as baseTest } from '@playwright/test';
+import { Page, test as baseTest } from '@playwright/test';
 import { setPage } from './PageFactory';
 
-baseTest.beforeEach(({ page }) => {
+/**
+ * A hook that runs before each test, setting the page context.
+ * @param {Page} page - The page context provided by Playwright.
+ */
+baseTest.beforeEach(({ page }: { page: Page }) => {
   setPage(page);
 });
 
+/**
+ * The base test object with a beforeEach hook already set up.
+ * This can be used to define tests with the page context set up.
+ * @type {import('@playwright/test').TestType}
+ */
 export const test = baseTest;
